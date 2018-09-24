8e98ae5b38e08756e7f5cb9681bca219f2a980e4
vladmakar <vladikmakar2745@gmail.com>
2018-09-24 10:57:43 -0400

    commiting lab 2

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<String> products = new List<string>(3);
+            products.Add("Laptop");
+            products.Add("Tablet");
+            products.Add("Desktop");
+            return View(products);
+        }
+
+        public ActionResult Details(String product)
+        {
+            ViewBag.Message = $"You selected product: [{product}]";
+
+            return View();
+        }
+    }
+}
